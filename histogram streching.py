6d38faf7cf56739c54d79386c8dd36bd931fd90a
Soundarya Srinidhi <74461779+Sound245939@users.noreply.github.com>
2020-12-30 23:42:07 +0100

    Update histogram streching.py

--- histogram streching.py
+++ histogram streching.py
@@ -1,1 +1,14 @@
+def hist_stretch(img_2):
 
+  #histogram stretching
+# Create zeros array to store the stretched image
+rows, cols = img_2.shape[:2]
+min=img_2.min()
+max=img_2.max()
+
+# Loop over the image and apply Min-Max formulae
+
+for i in range(0,rows):
+for j in range(0,cols):
+img3[i,j] = 255*(img3[i,j]-min)/(max-min)
+return img3
